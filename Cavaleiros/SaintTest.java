a17437a6a0dff44dc40011f65e3c33c1dc57039e
mateusforgiarini <mateus.silva@cwi.com.br>
2017-04-15 10:08:26 -0300

    feat (SaintTest) Implementar métodos que testam o projeto cavaleiro.

--- Cavaleiros/SaintTest.java
+++ Cavaleiros/SaintTest.java
@@ -1,35 +1,84 @@
-
-
 import static org.junit.Assert.*;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
-
-
 public class SaintTest{
     @Test
     public void vestirArmaduraDeixaArmaduraVestida(){
-        // AAA
-        // 1. Arrange - Montagem dos dados de teste
-        Armadura touro = new Armadura ("Touro", Categoria.OURO);
-        Saint personagem = new Saint ("Lala", touro);
-        // 2. Act  - Invocar a ação a  ser testada 
-        personagem.vestirArmadura();
-        // 3. Assert ´- Verificação dos resultados do teste
-        boolean resultado = personagem.getArmaduraVestida();
+        Armadura fenix = new Armadura ("Fênix", Categoria.BRONZE);
+        Saint ikki  = new Saint ("Ikki", fenix);
+        ikki.vestirArmadura();
+        boolean resultado = ikki.getArmaduraVestida();
         assertEquals(true, resultado);
     }
-    
     @Test
     public void naoVestirArmarduraDeixaArmaduraNaoVestida(){
-        Saint k = new Saint ("Hyoga", new Armadura ("Cisne", Categoria.BRONZE));
-        assertEquals (false, k.getArmaduraVestida());
+        Saint ionia = new Saint ("Ionia", new Armadura ("Capricónio", Categoria.OURO));
+        assertEquals (false, ionia.getArmaduraVestida());
     }
     @Test
     public void aoCriarSaintGeneroENaoInformado(){
-        Armadura virgem = new Armadura ("virgem", Categoria.OURO);
-        Saint mu = new Saint ("Mú", virgem);
-        assertEquals(Genero.NAO_INFORMADO, mu.getGenero());
+        Armadura cobra = new Armadura ("Cobra", Categoria.PRATA);
+        Saint shina = new Saint ("Shina", cobra);
+        assertEquals(Genero.NAO_INFORMADO, shina.getGenero());
+    }
+    @Test 
+    public void aoCriarSaintStatusVidaDeveSerVivo(){
+        Saint shaka = new Saint ("Shaka", new Armadura ("Virgem", Categoria.OURO));
+        assertEquals(StatusDeVida.VIVO, shaka.getStatusDeVida());
+    }
+    @Test 
+    public void retornaOValorStringDaCategoria(){
+        Saint shaka = new Saint ("Shaka", new Armadura ("Virgem", Categoria.OURO));
+        String valor = shaka.getValorStringDaCategoria();
+        int valorInt =  valor.compareTo("OURO");
+        assertEquals(0,valorInt);
+    }
+        @Test 
+    public void retornaOValorIntDaCategoria (){
+        Saint shion = new Saint ("Shion", new Armadura ("Áries", Categoria.OURO));
+        int valor = shion.getValorDaCategoria();
+        assertEquals (3, valor);
+    }
+    @Test 
+    public void iniciaBatalhaCavaleiroDeMenorCategoriaDevePederDezDeVida (){
+        Batalha b = new Batalha();
+        Saint seiya = new Saint ("Seiya", new Armadura ("Pégaso", Categoria.BRONZE));
+        Saint shion = new Saint ("Shion", new Armadura ("Áries", Categoria.OURO));
+        double delta  = 0.0000001;
+        b.iniciar(seiya, shion);
+        assertEquals (90, seiya.getVida(), delta );
+    }
+    @Test 
+    public void perdeVidaAoSofrerGolpe (){
+        Saint seiya = new Saint ("Seiya", new Armadura ("Pégaso", Categoria.BRONZE));
+        double delta  = 0.0000001;
+        seiya.perderVida(90);
+        assertEquals (10, seiya.getVida(), delta);
+    }
+    @Test 
+    public void deveRetonarONomeDoSaint (){
+        Saint seiya = new Saint ("Seiya", new Armadura ("Pégaso", Categoria.BRONZE));  
+        String nome = seiya.getNome();
+        int nomeInt = nome.compareTo("Seiya");
+        assertEquals(0, nomeInt);
+    }
+    @Test 
+    public void deveRetornaVidaDoSaint (){
+        Saint seiya = new Saint ("Seiya", new Armadura ("Pégaso", Categoria.BRONZE));  
+        double delta  = 0.0000001;
+        assertEquals(100,seiya.getVida(), delta);
+    }
+    @Test 
+    public void deveMudarOGeneroDoSaint (){
+        Saint seiya = new Saint ("Seiya", new Armadura ("Pégaso", Categoria.BRONZE));   
+        Genero genero = Genero.MASCULINO;
+        seiya.setGenero(genero);
+        assertEquals (Genero.MASCULINO,seiya.getGenero());
+    }
+    @Test 
+    public void retornaOGeneroDoSaint (){
+        Saint seiya = new Saint ("Seiya", new Armadura ("Pégaso", Categoria.BRONZE));   
+        assertEquals (Genero.NAO_INFORMADO,seiya.getGenero());
     }
 }
-    
