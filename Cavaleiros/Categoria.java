f517414303346b8be7e1bd7e00c27987099bf966
mateusforgiarini <mateus.silva@cwi.com.br>
2017-04-15 10:04:44 -0300

    feat (Categoria) - Implementar enum com possíveis categorias e métodos para acessá-las.

--- Cavaleiros/Categoria.java
+++ Cavaleiros/Categoria.java
@@ -1,14 +1,15 @@
 public enum Categoria {
     OURO(3), PRATA(2), BRONZE(1);
-    
-    private int valor;
-    
+    private int valor; 
+    private String valorString;
     private Categoria(int valor){
         this.valor = valor;
     }
     public int getValor(){
         return this.valor;
     }
+    public String getValorString (){
+        valorString = name();
+        return valorString;
+    }
 }
-
-
