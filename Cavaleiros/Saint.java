6ae9ea687211ae86e7a64bc210fc73b8595b24a3
mateusforgiarini <mateus.silva@cwi.com.br>
2017-04-15 10:00:00 -0300

    feat (Saint) Implementar métodos e atributos.

--- Cavaleiros/Saint.java
+++ Cavaleiros/Saint.java
@@ -3,24 +3,42 @@ public  class Saint{
     private Armadura armadura;
     private boolean armaduraVestida;
     private Genero genero = Genero.NAO_INFORMADO;
+    private StatusDeVida statusDeVida = StatusDeVida.VIVO;
+    private double vida = 100;
     public Saint (String nome, Armadura armadura){
       this.nome = nome;
       this.armadura = armadura;
     }
     public void vestirArmadura(){
         this.armaduraVestida = true;
-    }
-    
+    }    
     public boolean getArmaduraVestida(){
         return this.armaduraVestida;
-    }
-        
+    }     
     public Genero getGenero(){
         return this.genero;
     }
     public void setGenero (Genero genero){
         this.genero = genero;
-
+    }
+    public StatusDeVida getStatusDeVida(){
+        return this.statusDeVida;
+    }
+    public void perderVida(double golpe){
+        vida = vida - golpe;
+    }
+     public double getVida(){
+        return this.vida;
+    }
+    public int getValorDaCategoria (){
+        int valorDaCategoria = armadura.getValorDaCategoria();
+        return valorDaCategoria;
+    }
+    public String getValorStringDaCategoria(){
+        String valorString = armadura.getValorStringDaCategoria();
+        return valorString;
+    }
+    public String getNome(){
+        return this.nome;
     }
 }
-    
