e60fb5bdf04a17a54cf728d8ab9f8c4f3976888a
mateusforgiarini <mateus.silva@cwi.com.br>
2017-04-17 18:19:10 -0300

    feat (GoldSaint) Implementar classe GoldSaint.

---
+++ Cavaleiros/GoldSaint.java
@@ -0,0 +1,6 @@
+public class GoldSaint extends Saint {
+    public GoldSaint (String nome, Armadura armadura) throws Exception{
+        super(nome, armadura);
+        this.qtsSentidosDespertados = 7;
+    }
+}
