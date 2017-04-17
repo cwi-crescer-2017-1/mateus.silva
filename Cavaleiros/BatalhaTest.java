2628587b11be418f449b9b0afb5cd79b66581f1a
mateusforgiarini <mateus.silva@cwi.com.br>
2017-04-17 16:15:51 -0300

    feat(BatalhaTest) Implementar classe de teste para a classe batalha.

---
+++ Cavaleiros/BatalhaTest.java
@@ -0,0 +1,36 @@
+
+
+import static org.junit.Assert.*;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+public class BatalhaTest{
+     @Test 
+    public void iniciaBatalhaSaint1Perde10(){
+        Saint seiya = new Saint ("Seiya", new Armadura ("Pégaso", Categoria.BRONZE));
+        Saint shion = new Saint ("Shion", new Armadura ("Áries", Categoria.OURO));
+        Batalha b = new Batalha(seiya, shion);
+        double delta  = 0.0000001;
+        b.iniciar();
+        assertEquals (90, seiya.getVida(), delta );
+    }
+     @Test 
+    public void iniciaBatalhaSaint2Perde10(){
+        Saint shion = new Saint ("Shion", new Armadura ("Áries", Categoria.OURO));
+        Saint seiya = new Saint ("Seiya", new Armadura ("Pégaso", Categoria.BRONZE));
+        Batalha b = new Batalha( shion, seiya);
+        double delta  = 0.0000001;
+        b.iniciar();
+        assertEquals (90, seiya.getVida(), delta );
+    }
+     @Test 
+    public void iniciaBatalhaComDoisCavaleirosDaMesmaCategoriaSegundoCavaleiroDevePerder10 (){
+        Saint shion = new Saint ("Shion", new Armadura ("Áries", Categoria.OURO));
+        Saint mu = new Saint ("Mu", new Armadura ("Touro", Categoria.OURO));
+        Batalha b = new Batalha( shion, mu);
+        double delta  = 0.0000001;
+        b.iniciar();
+        assertEquals (90, mu.getVida(), delta );
+    }
+}
