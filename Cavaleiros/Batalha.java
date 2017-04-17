2287efb7851165a7ef12277f905fcc1c1880a285
mateusforgiarini <mateus.silva@cwi.com.br>
2017-04-17 16:14:50 -0300

    fix (Batalha) Corregir classe Batalha, métodos e atributos.

--- Cavaleiros/Batalha.java
+++ Cavaleiros/Batalha.java
@@ -1,5 +1,11 @@
 public class Batalha {   
-   public void iniciar (Saint saint1, Saint saint2){
+    private Saint saint1;
+    private Saint saint2;
+   public Batalha (Saint saint1, Saint saint2){
+        this.saint1 = saint1;
+        this.saint2 = saint2;
+    }
+   public void iniciar(){
        if (saint1.getValorDaCategoria () > saint2.getValorDaCategoria()){
            saint2.perderVida(10);
            System.out.println(saint1.getNome() +" (" +saint1.getValorStringDaCategoria() +") vs " +saint2.getNome() +" (" +saint2.getValorStringDaCategoria() +")");
