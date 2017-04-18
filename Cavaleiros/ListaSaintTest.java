7ae79e431fd3b55980eb91634ea931ed885cbe54
mateusforgiarini <mateus.silva@cwi.com.br>
2017-04-18 19:28:21 -0300

    feat (ListaSaint) Adicionar teste.

--- Cavaleiros/ListaSaintTest.java
+++ Cavaleiros/ListaSaintTest.java
@@ -7,11 +7,21 @@ import org.junit.Test;
 
 
 public class ListaSaintTest{
-    @Test
-    public void adicionarSaintALista() throws Exception{
+     @Test
+    public void adicionar1SaintALista() throws Exception{
          Saint saga = new Saint("Saga", new Armadura(new Constelacao("Gêmeos"), Categoria.OURO));
          ListaSaint lista = new ListaSaint();
          lista.adicionar(saga);
          assertEquals(saga, lista.get(0));
+    }
+    @Test
+    public void adicionar2SaintALista() throws Exception{
+         Saint saga = new Saint("Saga", new Armadura(new Constelacao("Gêmeos"), Categoria.OURO));
+         ListaSaint lista = new ListaSaint();
+         lista.adicionar(saga);
+         assertEquals(saga, lista.get(0));
+         GoldSaint mu = new GoldSaint ("Mu", new Armadura (new Constelacao ("Touro"), Categoria.OURO)); 
+         lista.adicionar(mu);
+         assertEquals(mu, lista.get(1));
     }
 }
