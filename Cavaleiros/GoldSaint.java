57042e6136e4823ecaefdf3d3524b756cdfc2e74
mateusforgiarini <mateus.silva@cwi.com.br>
2017-04-17 22:09:19 -0300

    fix (GoldSaint) Adicionar método que busca nome da constelação.

--- Cavaleiros/GoldSaint.java
+++ Cavaleiros/GoldSaint.java
@@ -2,7 +2,7 @@ public class GoldSaint extends Saint {
     public GoldSaint (String nome, Armadura armadura) throws Exception{
         super(nome, armadura);
         this.qtsSentidosDespertados = 7;
-        String constelacao = armadura.getConstelacao();
+        String constelacao = armadura.getConstelacao().getNomeDaConstelacao();
         if ( !constelacao.equals("Áries") 
         && !constelacao.equals("Touro")
         && !constelacao.equals("Gêmeos")
