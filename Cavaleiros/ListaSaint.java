9024371489c2843223a7b43da6d3c99ee841b0f3
mateusforgiarini <mateus.silva@cwi.com.br>
2017-04-18 19:04:56 -0300

    feat (ListaSaint) Implementar classe.

---
+++ Cavaleiros/ListaSaint.java
@@ -0,0 +1,7 @@
+import java.util.ArrayList;
+public class ListaSaint{
+    private ArrayList <Saint> listaSaint = new ArrayList();
+    public void adicionarSaint(Saint saint){
+         this.listaSaint.add(saint);
+    }
+ }
