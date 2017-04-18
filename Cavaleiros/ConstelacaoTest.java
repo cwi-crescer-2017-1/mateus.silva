046ee7600b242ac18a4bc3086e0b471124d3e3ca
mateusforgiarini <mateus.silva@cwi.com.br>
2017-04-18 12:20:38 -0300

    refactor (ConstelacaoTest) Arruamar  teste aoAdicionarGolpeEleVaiParaOFinalDoArray().

--- Cavaleiros/ConstelacaoTest.java
+++ Cavaleiros/ConstelacaoTest.java
@@ -7,8 +7,18 @@ public class ConstelacaoTest {
     public void aoAdicionarGolpeEleVaiParaOFinalDoArray() throws Exception{
         Constelacao virgem = new Constelacao ("Virgem");
         GoldSaint shaka = new GoldSaint ("Shaka", new Armadura (virgem, Categoria.OURO));
-        Golpe golpe = new Golpe ();
-        virgem.adicionarGolpe(golpe);
-        assertEquals(virgem.getGolpes()[2],golpe);
+        Golpe golpe1 = new Golpe ();
+        Golpe golpe2 = new Golpe ();
+        Golpe golpe3 = new Golpe ();
+        virgem.adicionarGolpe(golpe1);
+        assertEquals(virgem.getGolpes()[2],golpe1);
+        virgem.adicionarGolpe(golpe2);
+        assertEquals(virgem.getGolpes()[2],golpe2);
+        assertEquals(virgem.getGolpes()[1],golpe1);
+        virgem.adicionarGolpe(golpe3);
+        assertEquals(virgem.getGolpes()[2],golpe3);
+        assertEquals(virgem.getGolpes()[0],golpe1);
+        assertEquals(virgem.getGolpes()[1],golpe2);
+        
     }
 }
