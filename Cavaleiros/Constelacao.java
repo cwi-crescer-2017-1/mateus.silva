022ea5eafdde8adc711906f411d3780c1abd1ce6
mateusforgiarini <mateus.silva@cwi.com.br>
2017-04-18 16:51:34 -0300

    fix (Constelacao) Arrumar função.

--- Cavaleiros/Constelacao.java
+++ Cavaleiros/Constelacao.java
@@ -1,25 +1,12 @@
 public class Constelacao {
     private String nome;
     private Golpe []golpes = new Golpe [3];
+    private int ultimaPosicaoPreenchida = 0;
     public Constelacao (String nome){
         this.nome = nome;
     }
     public void adicionarGolpe(Golpe golpe){
-        if (golpes [2] == null){
-        golpes [2]  = golpe;}
-        else {
-             if (golpes [1] == null){
-                golpes [1] = golpes [2];
-                golpes [2]  = golpe;
-            }
-            else {
-                if (golpes [0] == null){
-                golpes [0] = golpes [1];
-                golpes [1] = golpes [2];
-                golpes [2]  = golpe;
-            }
-            }
-        }
+        golpes[ultimaPosicaoPreenchida++] = golpe;
     }
     public Golpe[] getGolpes (){
         return this.golpes; 
