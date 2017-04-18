3d025a69a1cbe030b3be45837ee6734681623e57
mateusforgiarini <mateus.silva@cwi.com.br>
2017-04-18 18:58:16 -0300

    refactor (ConstelacaoTest) Ajustar testes.

--- Cavaleiros/ConstelacaoTest.java
+++ Cavaleiros/ConstelacaoTest.java
@@ -4,29 +4,29 @@ import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 public class ConstelacaoTest{
-//@Test
- //   public void adicionarUmGolpe() {
- //       Constelacao gemeos = new Constelacao("Gêmeos");
- //       Golpe outraDimensao = new Golpe("Outra dimensão", 10);
- //       gemeos.adicionarGolpe(new Golpe("Outra dimensão", 10));
- //       ArrayList<Golpe> golpes = gemeos.getGolpes();
- //       assertEquals(outraDimensao, golpes.get(0));
+@Test
+    public void adicionarUmGolpe() {
+        Constelacao gemeos = new Constelacao("Gêmeos");
+        Golpe outraDimensao = new Golpe("Outra dimensão", 10);
+        gemeos.adicionarGolpe(new Golpe("Outra dimensão", 10));
+        ArrayList<Golpe> golpes = gemeos.getGolpes();
+        assertEquals(outraDimensao, golpes.get(0));
  //       assertNull(golpes.get(1));
 //        assertNull(golpes.get(2));
  //       // TODO: assert null
- //   }
-//@Test
- //   public void adicionarDoisGolpes() {
-  //      Constelacao gemeos = new Constelacao("Gêmeos");
-    //    Golpe outraDimensao = new Golpe("Outra dimensão", 10);
-      //  Golpe explosaoGalatica = new Golpe("Explosão Galáctica", 11);
-        //gemeos.adicionarGolpe(outraDimensao);
-       // gemeos.adicionarGolpe(explosaoGalatica);
-       // ArrayList<Golpe> golpes = gemeos.getGolpes();;
-       // assertEquals(outraDimensao, golpes.get(0));
-       // assertEquals(explosaoGalatica, golpes.get(1));
+   }
+@Test
+   public void adicionarDoisGolpes() {
+        Constelacao gemeos = new Constelacao("Gêmeos");
+        Golpe outraDimensao = new Golpe("Outra dimensão", 10);
+        Golpe explosaoGalatica = new Golpe("Explosão Galáctica", 11);
+        gemeos.adicionarGolpe(outraDimensao);
+        gemeos.adicionarGolpe(explosaoGalatica);
+        ArrayList<Golpe> golpes = gemeos.getGolpes();;
+        assertEquals(outraDimensao, golpes.get(0));
+        assertEquals(explosaoGalatica, golpes.get(1));
         //assertNull(golpes.get(2));
-    //}
+ }
 @Test
 
     public void adicionarTresGolpes() {
