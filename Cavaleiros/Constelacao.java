77e925a0c68400b8054118d41723a51806300207
mateusforgiarini <mateus.silva@cwi.com.br>
2017-04-19 14:26:28 -0300

    refactor (Constelacao) Otimizar classe.

--- Cavaleiros/Constelacao.java
+++ Cavaleiros/Constelacao.java
@@ -2,7 +2,6 @@ import java.util.ArrayList;
 public class Constelacao {
     private String nome;
     private ArrayList <Golpe> golpes= new ArrayList <Golpe> ();
-    private int ultimaPosicaoPreenchida = 0;
     public Constelacao (String nome){
         this.nome = nome;
     }
