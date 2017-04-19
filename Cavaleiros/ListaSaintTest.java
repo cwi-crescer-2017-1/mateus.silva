c1900cdc622aaa58c1e253961cc61268e227aaa9
mateusforgiarini <mateus.silva@cwi.com.br>
2017-04-19 00:12:42 -0300

    feat (ListaSaintTest) Implementar testes.

--- Cavaleiros/ListaSaintTest.java
+++ Cavaleiros/ListaSaintTest.java
@@ -45,4 +45,37 @@ public class ListaSaintTest{
          Saint saint = lista.buscarPorNome("Saga");
          assertEquals(true, saint.getNome().equals("Saga"));
     }
+    @Test
+    public void retornaSubListaDaCategoriaInformadaQueContem3SaintOuro() throws Exception{
+         GoldSaint saga = new GoldSaint("Saga", new Armadura(new Constelacao("Gêmeos"), Categoria.OURO));
+         GoldSaint mu = new GoldSaint("Mu", new Armadura(new Constelacao("Touro"), Categoria.OURO));
+         GoldSaint shaka = new GoldSaint("Shaka", new Armadura(new Constelacao("Virgem"), Categoria.OURO));
+         ListaSaint lista = new ListaSaint();
+         lista.adicionar(saga);
+         lista.adicionar(mu);
+         lista.adicionar(shaka);
+         Categoria categoria = Categoria.OURO;
+         ArrayList <Saint> subList =lista.buscarPorCategoria(categoria);
+         assertEquals(3, subList.size());
+    } 
+     @Test
+    public void retornaSubListaDaCategoriaInformadaQueContem1SaintBronze() throws Exception{
+         BronzeSaint shina = new BronzeSaint("Shina", new Armadura(new Constelacao("Cobra"), Categoria.BRONZE));
+         ListaSaint lista = new ListaSaint();
+         lista.adicionar(shina);
+         Categoria categoria = Categoria.BRONZE;
+         ArrayList <Saint> subList =lista.buscarPorCategoria(categoria);
+         assertEquals(1, subList.size());
+    }  
+     @Test
+    public void retornaSubListaDaCategoriaInformadaQueContem2SaintPrata() throws Exception{
+         SilverSaint seiya = new SilverSaint ("Seiya", new Armadura (new Constelacao ("Pégaso"), Categoria.PRATA)); 
+         SilverSaint hyoga = new SilverSaint("Hyoga", new Armadura(new Constelacao("Cisne"), Categoria.PRATA));
+         ListaSaint lista = new ListaSaint();
+         lista.adicionar(seiya);
+         lista.adicionar(hyoga);
+         Categoria categoria = Categoria.PRATA;
+         ArrayList <Saint> subList =lista.buscarPorCategoria(categoria);
+         assertEquals(2, subList.size());
+    }  
 }
