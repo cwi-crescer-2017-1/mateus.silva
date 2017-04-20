e6ae9ae849f95eeab039a2a5d948c95703312ace
mateusforgiarini <mateus.silva@cwi.com.br>
2017-04-20 00:17:18 -0300

    fix (ListaSaints) Ajustar método intersec.

--- Cavaleiros/ListaSaints.java
+++ Cavaleiros/ListaSaints.java
@@ -121,10 +121,10 @@ public class ListaSaints{
     public ArrayList<Saint> intersec (ArrayList <Saint>  lista){
         ArrayList <Saint> novaListaSaint = new ArrayList();
         int tamanhoDaLista1 = listaSaint.size();
-        for (int i = 0 ; i<listaSaint.size();i++){
-            Saint atual = listaSaint.get(i);
-            novaListaSaint.add(i,atual);
-        }
+        //for (int i = 0 ; i<listaSaint.size();i++){
+          //  Saint atual = listaSaint.get(i);
+           // novaListaSaint.add(i,atual);
+        //}
         for (int i = 0 ; i<lista.size();i++) {
             Saint atual = lista.get(i);
             boolean igual = false;
@@ -134,9 +134,9 @@ public class ListaSaints{
                    break;
                 } 
             }
-            if (igual == false){
+            if (igual == true){
                  novaListaSaint.add(atual);}
                     }
         return novaListaSaint;
     }
-}    
+ }
