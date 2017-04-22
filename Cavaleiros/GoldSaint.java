35fca042fca5807b8c448e8ff4d2001df9690937
mateusforgiarini <mateus.silva@cwi.com.br>
2017-04-21 22:40:14 -0300

    refactor (GoldSaint) mudar construtor.

--- Cavaleiros/GoldSaint.java
+++ Cavaleiros/GoldSaint.java
@@ -2,8 +2,7 @@ import java.util.ArrayList;
 public class GoldSaint extends Saint {
     public GoldSaint (String nome, String nomeDaConstelacao) throws Exception{
        
-        super(nome, nomeDaConstelacao);
-        this.armadura = new Armadura (new Constelacao (nomeDaConstelacao), Categoria.OURO);
+        super(nome, new Armadura(new Constelacao (nomeDaConstelacao), Categoria.OURO));
         this.qtsSentidosDespertados = 7;
         
         String constelacao = getArmadura().getConstelacao().getNomeDaConstelacao();
