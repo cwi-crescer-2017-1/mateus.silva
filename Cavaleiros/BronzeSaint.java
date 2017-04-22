c6a9a931f4942b22e5af90b734f7c89e80fea901
mateusforgiarini <mateus.silva@cwi.com.br>
2017-04-21 22:38:37 -0300

    refactor (BronzeSaint) mudar construtor.

--- Cavaleiros/BronzeSaint.java
+++ Cavaleiros/BronzeSaint.java
@@ -1,8 +1,7 @@
 import java.util.ArrayList;
 public class BronzeSaint extends Saint{
     public BronzeSaint(String nome, String nomeDaConstelacao) throws Exception{
-        super (nome,nomeDaConstelacao);
-        this.armadura = new Armadura (new Constelacao (nomeDaConstelacao), Categoria.BRONZE);
+        super(nome, new Armadura (new Constelacao (nomeDaConstelacao), Categoria.BRONZE));
         this.qtsSentidosDespertados = 5;
 }
 }
