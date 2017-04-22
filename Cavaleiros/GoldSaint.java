5ebd421c88515cc5bfc5c23c02631664e9786554
mateusforgiarini <mateus.silva@cwi.com.br>
2017-04-21 22:54:22 -0300

    refactor (GoldSaint) retirar throw new exception.

--- Cavaleiros/GoldSaint.java
+++ Cavaleiros/GoldSaint.java
@@ -1,25 +1,7 @@
 import java.util.ArrayList;
 public class GoldSaint extends Saint {
     public GoldSaint (String nome, String nomeDaConstelacao) throws Exception{
-       
         super(nome, new Armadura(new Constelacao (nomeDaConstelacao), Categoria.OURO));
         this.qtsSentidosDespertados = 7;
-        
-        String constelacao = getArmadura().getConstelacao().getNomeDaConstelacao();
-        if ( !constelacao.equals("Áries") 
-        && !constelacao.equals("Touro")
-        && !constelacao.equals("Gêmeos")
-        && !constelacao.equals("Câncer")
-        && !constelacao.equals("Virgem")
-        && !constelacao.equals("Leão")
-        && !constelacao.equals("Libra")
-        && !constelacao.equals("Escorpião")
-        && !constelacao.equals("Sagitário")
-        && !constelacao.equals("Capricórnio")
-        && !constelacao.equals("Aquário")
-        && !constelacao.equals("Peixes")) {
-            // dar erro
-            throw new Exception("Constelação inválida");
-        }
     }
 }
