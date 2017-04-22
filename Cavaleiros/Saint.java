a26533f9376a11a1953ffb2b2c5d115355f6f4d6
mateusforgiarini <mateus.silva@cwi.com.br>
2017-04-21 22:53:23 -0300

    refactor (Saint) lançar erro se armadura for de ouro e não tiver o nome das constelações  listadas.

--- Cavaleiros/Saint.java
+++ Cavaleiros/Saint.java
@@ -13,6 +13,25 @@ public abstract class Saint{
     public Saint (String nome, Armadura armadura) throws Exception{
       this.nome = nome;
       this.armadura = armadura;
+       
+      if (getValorDaCategoria() == 3){
+         String constelacao = armadura.getConstelacao().getNomeDaConstelacao();
+         if ( !constelacao.equals("Áries") 
+            && !constelacao.equals("Touro")
+            && !constelacao.equals("Gêmeos")
+            && !constelacao.equals("Câncer")
+            && !constelacao.equals("Virgem")
+            && !constelacao.equals("Leão")
+            && !constelacao.equals("Libra")
+            && !constelacao.equals("Escorpião")
+            && !constelacao.equals("Sagitário")
+            && !constelacao.equals("Capricórnio")
+            && !constelacao.equals("Aquário")
+            && !constelacao.equals("Peixes")) {
+            // dar erro
+            throw new Exception("Constelação inválida");
+           } 
+        }
     }
     public void vestirArmadura(){
         this.armaduraVestida = true;
