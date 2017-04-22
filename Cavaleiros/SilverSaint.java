6c44f227de94a27ffabd1046ae5732cd32252c52
mateusforgiarini <mateus.silva@cwi.com.br>
2017-04-21 22:39:59 -0300

    refactor (SilverSaint) mudar construtor.

--- Cavaleiros/SilverSaint.java
+++ Cavaleiros/SilverSaint.java
@@ -1,8 +1,7 @@
 import java.util.ArrayList;
 public class SilverSaint extends Saint {
     public SilverSaint (String nome, String nomeDaConstelacao) throws Exception{
-        super (nome, nomeDaConstelacao);
-        this.armadura = new Armadura (new Constelacao (nomeDaConstelacao), Categoria.PRATA);
+        super (nome, new Armadura (new Constelacao (nomeDaConstelacao), Categoria.PRATA));
         this.qtsSentidosDespertados = 6;
     }
 }
