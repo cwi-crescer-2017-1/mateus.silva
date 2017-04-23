aed776719eb387f508bbf933fe93c608f82ca057
mateusforgiarini <mateus.silva@cwi.com.br>
2017-04-23 10:31:17 -0300

    feat (BatalhaTest) implementar novos testes para método inciar da classe  batalha.

--- Cavaleiros/BatalhaTest.java
+++ Cavaleiros/BatalhaTest.java
@@ -33,4 +33,27 @@ public class BatalhaTest{
         assertEquals (0, mu.getVida(), delta );
         assertEquals (10, shion.getVida(), delta );
     }
+        @Test 
+    public void iniciaBatalhaComSaintsSemVida ()throws Exception{
+        Saint shion = new GoldSaint ("Shion", "Áries");
+        Saint mu = new GoldSaint ("Mu", "Touro");
+        shion.perderVida(100);
+        mu.perderVida(100);
+        Batalha b = new Batalha( shion, mu);
+        double delta  = 0.0000001;
+        b.iniciar();
+        assertEquals (0, mu.getVida(), delta );
+        assertEquals (0, shion.getVida(), delta );
+    }
+        @Test 
+    public void iniciaBatalha1SaintComMetadeDaVidaDoOutro ()throws Exception{
+        Saint shion = new GoldSaint ("Shion", "Áries");
+        Saint mu = new GoldSaint ("Mu", "Touro");
+        shion.perderVida(50);
+        Batalha b = new Batalha( shion, mu);
+        double delta  = 0.0000001;
+        b.iniciar();
+        assertEquals (50, mu.getVida(), delta );
+        assertEquals (0, shion.getVida(), delta );
+    }
 }
