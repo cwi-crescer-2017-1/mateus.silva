31e7dee75241052661ba8af0f24be6221f9762cd
mateusforgiarini <mateus.silva@cwi.com.br>
2017-04-23 20:51:55 -0300

    fix (BatalhaTest) Arrumar testes para método iniciar da classe batalha.

--- Cavaleiros/BatalhaTest.java
+++ Cavaleiros/BatalhaTest.java
@@ -4,56 +4,126 @@ import org.junit.Before;
 import org.junit.Test;
 public class BatalhaTest{
      @Test 
-    public void iniciaBatalhaSaint1Perde10()throws Exception{
-        Saint seiya = new BronzeSaint ("Seiya", "Pégaso");
+    public void iniciaBatalhaGoldSaintGoldPrata()throws Exception{
+        Saint seiya = new SilverSaint ("Seiya", "Pégaso");
         Saint shion = new GoldSaint ("Shion", "Áries");
+        
+        Golpe golpe1 = new Golpe ("Meteoro de Pégasus", 10);
+        Golpe golpe2 = new Golpe ("Cometa de Pégasus", 5);
+        Golpe golpe3 = new Golpe ("Chute de Pégasus", 2);
+        Golpe golpe4 = new Golpe ("Muralha de Cristal", 10);
+        Golpe golpe5 = new Golpe ( "Extinção Estelar", 7);
+        Golpe golpe6 = new Golpe ("Rede de Cristal", 8); 
+        seiya.aprenderGolpe(golpe1);
+        seiya.aprenderGolpe(golpe2);
+        seiya.aprenderGolpe(golpe3);
+        shion.aprenderGolpe(golpe4);
+        shion.aprenderGolpe(golpe5);
+        shion.aprenderGolpe(golpe6);
+        
+        Golpear golpear1 = new Golpear (seiya, shion);
+        Golpear golpear2 = new Golpear (shion, seiya);
+        VestirArmadura seiyaVestirArmadura = new VestirArmadura (seiya);
+        VestirArmadura shionVestirArmadura = new VestirArmadura (shion);
+        
+       
+        seiya.adicionarMovimento(golpear1);
+        seiya.adicionarMovimento(seiyaVestirArmadura);
+        shion.adicionarMovimento(golpear2);
+        shion.adicionarMovimento(shionVestirArmadura);
         Batalha b = new Batalha(seiya, shion);
         double delta  = 0.0000001;
         b.iniciar();
         assertEquals (0, seiya.getVida(), delta );
-        assertEquals (10, shion.getVida(), delta );
+        assertEquals (69, shion.getVida(), delta );
     }
      @Test 
-    public void iniciaBatalhaSaint2Perde10()throws Exception{
+    public void iniciaBatalhaGoldSaintBronzeSaint()throws Exception{
+        Saint seiya = new BronzeSaint ("Seiya", "Pégaso");
         Saint shion = new GoldSaint ("Shion", "Áries");
-        Saint seiya = new BronzeSaint ("Seiya","Pégaso");
-        Batalha b = new Batalha( shion, seiya);
+        Golpe golpe1 = new Golpe ("Meteoro de Pégasus", 10);
+        Golpe golpe2 = new Golpe ("Rede de Cristal", 8); 
+        seiya.aprenderGolpe(golpe1);
+        shion.aprenderGolpe(golpe2);
+        Golpear golpear1 = new Golpear (seiya, shion);
+        Golpear golpear2 = new Golpear (shion, seiya);
+        VestirArmadura seiyaVestirArmadura = new VestirArmadura (seiya);
+        VestirArmadura shionVestirArmadura = new VestirArmadura (shion);
+        seiya.adicionarMovimento(golpear1);
+        seiya.adicionarMovimento(seiyaVestirArmadura);
+        shion.adicionarMovimento(golpear2);
+        shion.adicionarMovimento(shionVestirArmadura);
+        Batalha b = new Batalha(seiya, shion);
         double delta  = 0.0000001;
         b.iniciar();
         assertEquals (0, seiya.getVida(), delta );
-        assertEquals (10, shion.getVida(), delta );
+        assertEquals (50, shion.getVida(), delta );
     }
      @Test 
-    public void iniciaBatalhaComDoisCavaleirosDaMesmaCategoriaSegundoCavaleiroDevePerder10 ()throws Exception{
-        Saint shion = new GoldSaint ("Shion", "Áries");
+    public void iniciaBatalhaComDoisCavaleirosDaMesmaCategoria()throws Exception{
         Saint mu = new GoldSaint ("Mu", "Touro");
-        Batalha b = new Batalha( shion, mu);
+        Saint shion = new GoldSaint ("Shion", "Áries");
+        Golpe golpe1 = new Golpe ("Rede de Cristal", 15);
+        Golpe golpe2 = new Golpe ("Chifrada", 20); 
+        shion.aprenderGolpe(golpe1);
+        mu.aprenderGolpe(golpe2);
+        Golpear golpear1 = new Golpear (shion, mu);
+        Golpear golpear2 = new Golpear (mu, shion);
+        VestirArmadura shionVestirArmadura = new VestirArmadura (mu);
+        VestirArmadura muVestirArmadura = new VestirArmadura (shion);
+        shion.adicionarMovimento(golpear1);
+        shion.adicionarMovimento(shionVestirArmadura);
+        mu.adicionarMovimento(golpear2);
+        mu.adicionarMovimento(muVestirArmadura);
+        Batalha b = new Batalha(mu, shion);
         double delta  = 0.0000001;
         b.iniciar();
-        assertEquals (0, mu.getVida(), delta );
-        assertEquals (10, shion.getVida(), delta );
-    }
-        @Test 
-    public void iniciaBatalhaComSaintsSemVida ()throws Exception{
-        Saint shion = new GoldSaint ("Shion", "Áries");
-        Saint mu = new GoldSaint ("Mu", "Touro");
-        shion.perderVida(100);
-        mu.perderVida(100);
-        Batalha b = new Batalha( shion, mu);
-        double delta  = 0.0000001;
-        b.iniciar();
-        assertEquals (0, mu.getVida(), delta );
+        assertEquals (85, mu.getVida(), delta );
         assertEquals (0, shion.getVida(), delta );
     }
         @Test 
-    public void iniciaBatalha1SaintComMetadeDaVidaDoOutro ()throws Exception{
-        Saint shion = new GoldSaint ("Shion", "Áries");
-        Saint mu = new GoldSaint ("Mu", "Touro");
-        shion.perderVida(50);
-        Batalha b = new Batalha( shion, mu);
-        double delta  = 0.0000001;
-        b.iniciar();
-        assertEquals (50, mu.getVida(), delta );
-        assertEquals (0, shion.getVida(), delta );
+    public void iniciaBatalhaSilverBronze ()throws Exception{
+       Saint seiya = new SilverSaint ("Seiya", "Pégaso");
+       Saint shina = new BronzeSaint ("Shina", "Cobra");
+       Golpe golpe1 = new Golpe ("Meteoro de Pégasus", 10);
+       Golpe golpe2 = new Golpe ("Cobra", 8); 
+       seiya.aprenderGolpe(golpe1);
+       shina.aprenderGolpe(golpe2);
+       Golpear golpear1 = new Golpear (seiya, shina);
+       Golpear golpear2 = new Golpear (shina, seiya);
+       VestirArmadura seiyaVestirArmadura = new VestirArmadura (seiya);
+       VestirArmadura shinaVestirArmadura = new VestirArmadura (shina);
+       seiya.adicionarMovimento(golpear1);
+       seiya.adicionarMovimento(seiyaVestirArmadura);
+       shina.adicionarMovimento(golpear2);
+       shina.adicionarMovimento(shinaVestirArmadura);
+       Batalha b = new Batalha(seiya, shina);
+       double delta  = 0.0000001;
+       b.iniciar();
+       assertEquals (60, seiya.getVida(), delta );
+       assertEquals (0, shina.getVida(), delta );
+    }
+        @Test 
+    public void iniciaBatalha1SaintSemVida ()throws Exception{
+       Saint seiya = new SilverSaint ("Seiya", "Pégaso");
+       Saint shina = new BronzeSaint ("Shina", "Cobra");
+       Golpe golpe1 = new Golpe ("Meteoro de Pégasus", 10);
+       Golpe golpe2 = new Golpe ("Cobra", 8); 
+       seiya.aprenderGolpe(golpe1);
+       shina.aprenderGolpe(golpe2);
+       Golpear golpear1 = new Golpear (seiya, shina);
+       Golpear golpear2 = new Golpear (shina, seiya);
+       VestirArmadura seiyaVestirArmadura = new VestirArmadura (seiya);
+       VestirArmadura shinaVestirArmadura = new VestirArmadura (shina);
+       seiya.adicionarMovimento(golpear1);
+       seiya.adicionarMovimento(seiyaVestirArmadura);
+       shina.adicionarMovimento(golpear2);
+       shina.adicionarMovimento(shinaVestirArmadura);
+       Batalha b = new Batalha(seiya, shina);
+       double delta  = 0.0000001;
+       seiya.perderVida(100);
+       b.iniciar();
+       assertEquals (0, seiya.getVida(), delta );
+       assertEquals (100, shina.getVida(), delta );
     }
 }
