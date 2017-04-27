020e8591af5e749296c5228d6b391165199912af
mateusforgiarini <mateus.silva@cwi.com.br>
2017-04-26 23:45:36 -0300

    refactor (SorteDoDia) refatorar testes.

--- Cavaleiros/SorteDoDiaTest.java
+++ Cavaleiros/SorteDoDiaTest.java
@@ -8,25 +8,29 @@ public class SorteDoDiaTest
 {
    @Test
     public void testaParaSaberResultadosEsperados(){
-        SorteDoDia sorte = new SorteDoDia(new DadoD6());
-        boolean resultado = sorte.estouComSorte();
+        SorteDoDia sorte = new SorteDoDia(new Dado3());
+        boolean resultado = sorte.sortear();
         boolean esperado = resultado && resultado;
         assertEquals (esperado, resultado);    
    }
    @Test
     public void testaParaSaberSeEstouComSorteRetornaFalse(){
-        SorteDoDia sorte = new SorteDoDia(new DadoD6());
-        boolean resultado = sorte.estouComSorte();
+        SorteDoDia sorte = new SorteDoDia(new Dado3());
+        boolean resultado = sorte.sortear();
         while (resultado == false){
-                resultado = sorte.estouComSorte();}
+                resultado = sorte.sortear();}
         assertEquals (true, resultado);    
-   }
-    @Test
-    public void testaParaSaberSeEstouComSorteRetornaTrue(){
-        SorteDoDia sorte = new SorteDoDia(new DadoD6());
-        boolean resultado = sorte.estouComSorte();
-        while (resultado == true){
-                resultado = sorte.estouComSorte();}   
-        assertEquals (false, resultado);    
-   }
+    }
+   @Test
+    public void estouComSorte() {
+        Sorteador sorteador = new DadoFalso(3);
+        SorteDoDia sorte = new SorteDoDia(sorteador);
+        assertTrue(sorte.sortear());
+    }
+   @Test
+    public void estouSemSorte() {
+        Sorteador sorteador = new DadoFalso(1);
+        SorteDoDia sorte = new SorteDoDia(sorteador);
+        assertFalse(sorte.sortear());
+    }
 }
