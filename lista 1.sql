23f9be012e9b9ceb66a1bc143f19081dc15d04b4
mateusforgiarini <mateus.silva@cwi.com.br>
2017-04-28 16:18:15 -0300

    refactor (Lista1) Alinhar colunas da tabela produto.

--- lista 1.sql
+++ lista 1.sql
@@ -7,13 +7,13 @@ insert into CidadeAux  select* from Cidade where UF = 'RS';
 select*from CidadeAux
 drop table produto 
 create table Produto (
-Codigo int identity not null,
-NomeCurto varchar(14) not null,
-NomeDescritivo varchar(20) not null,
-DataCriacao varchar(10) not null,
-LocalNoEstoque varchar(5) not null ,
-Quantidade varchar(10) not null,
-Preco decimal(9,2) not null ,
+Codigo          int identity   not null,
+NomeCurto       varchar(14)    not null,
+NomeDescritivo  varchar(20)    not null,
+DataCriacao     varchar(10)    not null,
+LocalNoEstoque  varchar(5)     not null,
+Quantidade      varchar(10)    not null,
+Preco           decimal(9,2)   not null,
 constraint PK_Produto primary key (codigo))
 
 insert into Produto 
