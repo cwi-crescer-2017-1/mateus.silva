e5ceb50072d823b68bc63aa8a078bd6ea2825f2c
mateusforgiarini <mateus.silva@cwi.com.br>
2017-04-30 19:07:17 -0300

    feat (Lista3) Implementar ranking.

--- Modulo 2/lista3.sql
+++ Modulo 2/lista3.sql
@@ -1,3 +1,4 @@
+    --Datas
 select IDempregado as ID,
 	 NomeEmpregado as Nome,
 	 datediff(year,DataNascimento, dataAdmissao) - 
@@ -9,3 +10,13 @@ select IDempregado as ID,
 	 DataAdmissao as  [Admissão]
 	 from empregado
 	 where  year (DataAdmissao) = 1980
+
+	--Ranking
+
+	 
+	 select top 1 cargo, count(1) as total 
+	 from empregado
+	 group by cargo
+	 order by total desc
+	
+   
