c78ca83f7b8d8bfe1211ad607ff1e10f6447dcad
mateusforgiarini <mateus.silva@cwi.com.br>
2017-05-01 17:54:00 -0300

    feat (Lista4) implementar finais de semana.

--- Modulo 2/lista4.sql
+++ Modulo 2/lista4.sql
@@ -16,3 +16,20 @@ from licitacao
 group by Empresa_Licitante
 order by [Total Faturamento] desc
 
+-- Top Cidades 
+
+select top 10 municipio as [Cidade],
+sum(Imposto_Federal + Imposto_Estadual + Imposto_Municipal)as [Total de Impostos Arrecadados]
+from licitacao 
+group by municipio 
+order by  [Total de Impostos Arrecadados] desc 
+
+
+
+--Finais de semana
+
+select  projeto as [Projetos] 
+from licitacao 
+where datepart(WEEKDAY, data_inicio_real) = 1 
+or datepart(WEEKDAY, data_inicio_real) =7
+
