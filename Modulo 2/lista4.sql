a615d27498a5b0b1b54509f844657948054d963d
mateusforgiarini <mateus.silva@cwi.com.br>
2017-05-02 23:06:28 -0300

    feat (Lista4) Implementar reajuste salarial.

---
+++ Modulo 2/lista4.sql
@@ -0,0 +1,29 @@
+
+-- Múltiplos acessos
+
+select e.nomeEmpregado as [nome do empregado],
+		e.iddepartamento as [departamento],
+        g.nomeEmpregado as [nome do gerente],
+		g.iddepartamento as [departamento]
+	    from    empregado e 
+	    inner join empregado g on e.idgerente = g.idempregado
+	 
+--Maior Salário
+
+ select top 1 e.iddepartamento , d.nomedepartamento as [nome do departamento]
+ from departamento d
+ right join empregado e on   d.iddepartamento = e.iddepartamento 
+ order by e.salario desc
+ 
+-- Reajuste salarial
+
+select*into copiaempregado from empregado 
+
+begin transaction 
+update empregado
+set salario = salario*1.173
+where iddepartamento in (select iddepartamento from departamento where localizacao = 'SAO PAULO')
+
+commit 
+go 
+
