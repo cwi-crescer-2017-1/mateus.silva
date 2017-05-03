f758b2b91d672faa982269461d0201d08e1da641
mateusforgiarini <mateus.silva@cwi.com.br>
2017-05-03 10:15:07 -0300

    feat (lista5) implementar reajuste salarial e cidades duplicadas.

--- Modulo 2/lista5.sql
+++ Modulo 2/lista5.sql
@@ -15,3 +15,12 @@ select e.nomeEmpregado as [nome do empregado],
  right join empregado e on   d.iddepartamento = e.iddepartamento 
  order by e.salario desc
  
+ -- Reajuste salarial 
+select*into copiaempregado from empregado  
+begin transaction 
+update empregado
+set salario = salario*1.173
+where iddepartamento in (select iddepartamento from departamento where localizacao = 'SAO PAULO')
+commit 
+go  -- Cidades duplicadas
+select Nome, UF from cidade where nome   in (  select nome from cidade   group by nome, uf  having count(*)>1 ) 
