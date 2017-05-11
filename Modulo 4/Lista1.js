f9b9540c59a4af89d26cbef90fc7504822c43c02
mateusforgiarini <mateus.silva@cwi.com.br>
2017-05-11 18:55:57 -0300

    feat (Lista1) Implementar exercício 1.

---
+++ Modulo 4/Lista1.js
@@ -0,0 +1,13 @@
+//Exercício 1
+function daisyGame(a) {
+  if (a%2 ===1) {
+    return "Love me";
+  }else {
+    return "Love me not";}
+  }
+console.log(daisyGame(1))
+console.log(daisyGame(2))
+console.log(daisyGame(3))
+console.log(daisyGame(4))
+
+//Exercício 2
