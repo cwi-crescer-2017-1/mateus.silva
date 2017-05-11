c504869c311ff7f0fbc96f2673b1c51a8da8ed90
mateusforgiarini <mateus.silva@cwi.com.br>
2017-05-11 19:14:59 -0300

    feat (Lista1) implementar exercício 2.

--- Modulo 4/Lista1.js
+++ Modulo 4/Lista1.js
@@ -1,4 +1,5 @@
 //Exercício 1
+
 function daisyGame(a) {
   if (a%2 ===1) {
     return "Love me";
@@ -10,4 +11,21 @@ console.log(daisyGame(2))
 console.log(daisyGame(3))
 console.log(daisyGame(4))
 
+//------------------------------------------------------------
 //Exercício 2
+
+function maiorTexto(array) {
+for (var i = 0; i<array.length; i++)
+     var string =  array[i];
+     var maiorString = 0;
+     if (string.length> maiorString){
+       maiorString = string;
+     }
+
+   return maiorString;
+ }
+ console.log(maiorTexto(array = ["lala", "lalala", "lalalala"]));
+ console.log(maiorTexto(array = ["lala", "lalala"]));
+
+//------------------------------------------------------------
+//Exercício 3
