328847ae1de6626e2602900cbaa8b0fa8acb8ed3
mateusforgiarini <mateus.silva@cwi.com.br>
2017-05-15 22:22:47 -0300

    refactor (Ex1 lista2) otimizar código.

--- Modulo 4/js/exercicios-arrays.js
+++ Modulo 4/js/exercicios-arrays.js
@@ -2,19 +2,16 @@ console.log(series);
 //------------------------------------------------------------------------
 //Exercício 1
 function seriesInvalidas(series){
- data  = new Date();
- ano = data.getFullYear();
- var invalidas = " ";
- for (var i = 0; i <series.length; i++){
-   var arrayAtributos = [series[i].diretor, series[i].distribuidora, series[i].elenco, series[i].genero, series[i].temporadas,
-    series[i].titulo,  series[i].numeroEpisodios];
-   for (var a = 0; a <arrayAtributos.length; a++){
-     if (series[i].anoEstreia>ano ||
-        typeof arrayAtributos[a] ==="undefined" ||  arrayAtributos[a]  === null){
-		          invalidas+=  series[i].titulo +" - ";
-             break;}
-       }
- }
+    ano = new Date().getFullYear();
+    var invalidas = "";
+    series.forEach(serie =>  {
+         let algumCampoInvalido = Object.values(serie).some(valor => {
+         let valorInvalido = (typeof  valor  ==="undefined" ||  valor=== null);
+         let anoInvalido = serie.anoEstreia>ano;
+         return valorInvalido || anoInvalido;});
+         if(algumCampoInvalido){
+         invalidas+= serie.titulo + " - ";}
+    })
 return "Séries inválidas : " + invalidas.substring(invalidas.length-2, 0);
 }
 console.log(seriesInvalidas(series));
