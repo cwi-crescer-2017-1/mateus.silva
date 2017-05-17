dad753b334b8207c32d647b4a62a2d4b75593d9c
mateusforgiarini <mateus.silva@cwi.com.br>
2017-05-17 17:26:12 -0300

    refactor (dia1) melhorar código.

--- Modulo 5/index.js
+++ Modulo 5/index.js
@@ -1,5 +1,5 @@
 var app = angular.module('poke', []);
 app.controller('controller1', function($scope) {
-     var pokemon = {nome: 'pikachu'};
+     var pokemon = {nome: 'pikachu', tipo:'elétrico' };
    $scope.pokemon= pokemon;
   })
