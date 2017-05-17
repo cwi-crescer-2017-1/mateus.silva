34a498d833ca09324c44e8364d3425cf109337f8
mateusforgiarini <mateus.silva@cwi.com.br>
2017-05-17 18:32:26 -0300

    feat (dia1) adicionar exercício com li.

--- Modulo 5/index.js
+++ Modulo 5/index.js
@@ -1,5 +1,13 @@
 var app = angular.module('poke', []);
 app.controller('controller1', function($scope) {
-     var pokemon = {nome: '', tipo:'' };
-   $scope.pokemon= pokemon;
+     var pokemons = [{nome: 'Pokémon 1', tipo:'fogo' }, {nome: 'Pokémon 2', tipo:'água' },
+     {nome: 'Pokémon 3', tipo:'elétrico' }];
+   $scope.pokemons= pokemons;
   })
+
+//  Ex. anterior
+//   var app = angular.module('poke', []);
+//    app.controller('controller1', function($scope) {
+//       var pokemon = {nome: '', tipo:'' };
+//     $scope.pokemon= pokemon;
+//    })
