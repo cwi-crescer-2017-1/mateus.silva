4727380b97e4ec06b21775cd5a5c92ceac1a02e4
mateusforgiarini <mateus.silva@cwi.com.br>
2017-05-17 02:33:55 -0300

    feat (Estatísticas) adicionar estatísticas.

--- Modulo 4/js/pokeapi.js
+++ Modulo 4/js/pokeapi.js
@@ -30,7 +30,10 @@ document.addEventListener('DOMContentLoaded', function() {
        ul.append(li)
        let arrayTypes = json.types;
        arrayTypes.forEach( t => {li.appendChild(document.createTextNode(t.type.name));})
-
+       arrayStats = json.stats;
+       console.log(arrayStats)
+       arrayStats.forEach ( s => { var stats = document.createElement('p'); stats.textContent =  s.stat.name.toUpperCase(); div.append(stats);
+              var barra = document.createElement('progress'); barra.value= s.base_stat; barra.max = 100; div.append(barra)  })
     })
 }
 })
