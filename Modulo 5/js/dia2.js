757ea305568e2cf75409f6c3150f4674d2a657fb
mateusforgiarini <mateus.silva@cwi.com.br>
2017-05-18 20:11:12 -0300

    feat (dia2) implementar mascada.

--- Modulo 5/js/dia2.js
+++ Modulo 5/js/dia2.js
@@ -1,7 +1,53 @@
-var modulo = angular.module('data', []);
-modulo.controller('controller1', function($scope) {
-$scope.imprime = function () {
 
-    $scope.data = new Date ($scope.date);
-}
+var modulo = angular.module('data', []);
+
+modulo.filter('mascada', function () {
+  return function (nome) {
+    return nome.replace(/(nunes)/i, "$ $1 $");
+  };
 });
+
+modulo.controller('controller1', function($scope, $filter) {
+
+$scope.imprime = function () {
+  $scope.data = new Date ($scope.date);
+}
+
+  let instrutores = [{
+    nome: 'Bernardo',
+    aula: [{
+        numero: 1,
+        nome: 'OO'
+      },
+      {
+        numero: 4,
+        nome: 'Javascript'
+      }
+    ]
+  },
+  {
+    nome: 'Nunes',
+    aula: [{
+      numero: 2,
+      nome: 'Banco de Dados I'
+    }]
+  },
+  {
+    nome: 'Pedro (PHP)',
+    aula: [{
+      numero: 3,
+      nome: 'HTML e CSS'
+    }]
+  },
+  {
+    nome: 'Zanatta',
+    aula: [{
+      numero: 5,
+      nome: 'AngularJS'
+    }]
+  }
+];
+
+$scope.instrutores = instrutores;
+console.log($filter('mascada')('teste nunes teste'));
+})
