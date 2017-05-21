ebbd50b72ed97f4e56916052c4300b312942a596
mateusforgiarini <mateus.silva@cwi.com.br>
2017-05-21 18:22:23 -0300

    feat (crud) adicionar imagens.

--- Modulo 5/CRUD/crud.js
+++ Modulo 5/CRUD/crud.js
@@ -4,36 +4,85 @@ var modulo = angular.module ("myApp", []);
 modulo.controller("mainController", function ($scope, $filter){
 
 $scope.aulas =
-{
+[{
    id: 0,
    nome: "OO"
-     };
+ },
+ {
+    id: 1,
+    nome: "Banco de Dados"
+  },
+  {
+     id: 2,
+     nome: "HTML"
+   },
+   {
+      id: 3,
+      nome: "JavaScript"
+    },
+
+    {
+       id: 4,
+       nome: "AngularJS"
+     }
+];
 $scope.instrutores =[{
     id: 0,                            // Gerado
     nome: "Bernardo",                     // Obrigatório (length = min 3, max 20)
     sobrenome: "Rezende",           // Opcional (length = max 30)
-    idade: 25,                        // Obrigatório (max 90)
+    idade: 30,                        // Obrigatório (max 90)
     email: "bernardo@cwi.com.br",        // Obrigatório (type=email)
-    dandoAula: true,                  // true ou false
-    aula: [1, 4],                     // Opcional (array)
-    urlFoto: "Bernardo.png"  // Opcional (porém tem uma default de livre escolha)
-  }];
+    dandoAula: false,                  // true ou false
+    aula: [0, 3],                     // Opcional (array)
+    urlFoto: "img/Bernardo.png"  // Opcional (porém tem uma default de livre escolha)
+  },
+  {
+      id: 1,                            // Gerado
+      nome: "André",                     // Obrigatório (length = min 3, max 20)
+      sobrenome: "Nunes",           // Opcional (length = max 30)
+      idade: 30,                        // Obrigatório (max 90)
+      email: "andre@cwi.com.br",        // Obrigatório (type=email)
+      dandoAula: false,                  // true ou false
+      aula: [1],                     // Opcional (array)
+      urlFoto: "img/nunes.png"  // Opcional (porém tem uma default de livre escolha)
+    },
+    {
+        id: 2,                            // Gerado
+        nome: "Pedro",                     // Obrigatório (length = min 3, max 20)
+        sobrenome: "Henrique Pires",           // Opcional (length = max 30)
+        idade: 20,                        // Obrigatório (max 90)
+        email: "php@cwi.com.br",        // Obrigatório (type=email)
+        dandoAula: false,                  // true ou false
+        aula: [2],                     // Opcional (array)
+        urlFoto: "img/php.png"  // Opcional (porém tem uma default de livre escolha)
+      },
+      {
+          id: 3,                            // Gerado
+          nome: "Everton",                     // Obrigatório (length = min 3, max 20)
+          sobrenome: "Zanatta",           // Opcional (length = max 30)
+          idade: 27,                        // Obrigatório (max 90)
+          email: "everton@cwi.com.br",        // Obrigatório (type=email)
+          dandoAula: true,                  // true ou false
+          aula: [4],                     // Opcional (array)
+          urlFoto: "img/zanatta.png"  // Opcional (porém tem uma default de livre escolha)
+        }];
 
- $scope.todasAsAulas = [$scope.aulas];
 
- var operadorLogico = 1;
+
+
 
  function procurarPorNome () {
-    return $scope.todasAsAulas.some (aula => aula.nome.toUpperCase().includes($scope.nome.toUpperCase()));
+    return $scope.aulas.some (aula => aula.nome.toUpperCase().includes($scope.nome.toUpperCase()));
  }
+
+
 
 $scope.incluir = function (){
    if (procurarPorNome()){
       return alert("Aula já cadastrada.");}
    if ($scope.myForm1.$valid){
-      $scope.novaAula = {id:operadorLogico, nome: $scope.nome}
-      $scope.todasAsAulas.push(angular.copy($scope.novaAula));
-      operadorLogico++;
+      $scope.novaAula = {id:$scope.aulas.length, nome: $scope.nome}
+      $scope.aulas.push(angular.copy($scope.novaAula));
       $scope.novaAula = {};
 }
 }
@@ -47,12 +96,12 @@ $scope.mudar = function (){
 
 
 function procurarAula (index) {
-    return $scope.instrutores.some (instrutor => instrutor.aula.includes($scope.todasAsAulas[index].nome));
+    return $scope.instrutores.some (instrutor => instrutor.aula.includes($scope.aulas[index].nome));
     }
 
 
 function filtrarPorNome() {
-     var arrayCom1objeto = $scope.todasAsAulas.filter(function(aula){ return aula.nome===$scope.list})
+     var arrayCom1objeto = $scope.aulas.filter(function(aula){ return aula.nome===$scope.list})
      return arrayCom1objeto;
 }
 
@@ -60,23 +109,31 @@ $scope.remover= function (index){
    if (procurarAula(index)){
      return alert("Não é possível excluir esta aula. Está sendo utilizada.");
    }
-  $scope.todasAsAulas.splice(index +1, 1);
+  $scope.aulas.splice(index +1, 1);
   }
 
- var operadorLogicoInstrutor = 1;
+
      $scope.incluirInstrutor = function (){
      if (procurarPorInstrutor()){
         return alert("Instrutor  já cadastrado.");}
-     if ($scope.myForm2.$valid){
-        $scope.novoInstrutor = {id:operadorLogicoInstrutor, nome: $scope.nomeInstrutor, sobrenome: $scope.sobrenomeInstrutor,
+     if (procurarPorEmail()){
+       return alert("Email já está sendo utilizado.")
+     }
+     if (typeof $scope.imgInstrutor==="undefined"){
+       $scope.imgInstrutor = "img/Default.png";
+     }
+     if  ($scope.myForm2.$valid){
+        $scope.novoInstrutor = {id:$scope.instrutores.length, nome: $scope.nomeInstrutor, sobrenome: $scope.sobrenomeInstrutor,
         idade: $scope.idadeInstrutor, email:$scope.emailInstrutor, dandoAula: $scope.dandoAula, aula: $scope.aulaInstrutor, urlFoto: $scope.imgInstrutor}
         $scope.instrutores.push(angular.copy($scope.novoInstrutor));
-        operadorLogicoInstrutor++;
         $scope.novoInstrutor = {};
      }
   }
   function procurarPorInstrutor () {
      return $scope.instrutores.some (instrutor=> instrutor.nome.toUpperCase().includes($scope.nomeInstrutor.toUpperCase()));
+  }
+  function procurarPorEmail () {
+     return $scope.instrutores.some (instrutor=> instrutor.email.toUpperCase().includes($scope.emailInstrutor.toUpperCase()));
   }
 
     $scope.mudarInstrutor = function (){
