dd320c4340c32d7c3d3219eedbf53502cff8fc9d
mateusforgiarini <mateus.silva@cwi.com.br>
2017-05-28 10:07:26 -0300

    feat (chat) adicionar função que deixa a scrollbar sempre na posição bottom.

--- Modulo 6/DEV/dotnet/Chat/ChatHTML/chat.js
+++ Modulo 6/DEV/dotnet/Chat/ChatHTML/chat.js
@@ -1,5 +1,8 @@
 modulo.filter("mascada", function (){
   return function (texto){
+    if (texto ==="undefined"){
+      return;
+    }
     if (texto.includes("nunes")){
       return texto.replace(/(nunes)/i, "$$$$$$$$$ $$$$$$$$$")
     }
@@ -11,17 +14,21 @@ modulo.filter("mascada", function (){
 modulo.controller("mainController", function ($scope, $filter, chatService){
 
 
+setInterval(function (){
+      var scroll= document.getElementById("main");
+      scroll.scrollTop = scroll.scrollHeight;
+      buscarMensagens()}, 2000);
+
+
   $scope.enviarMensagem = function (input){
-  chatService.enviarMensagem(input).then(function (response){
-    buscarMensagens();
+      chatService.enviarMensagem(input).then(function (response){
+      buscarMensagens();
     })};
 
 
-setInterval(buscarMensagens(), 3000);
 
-
-    function buscarMensagens (){
-  chatService.buscarMensagens().then(function(response){
+  function buscarMensagens (){
+      chatService.buscarMensagens().then(function(response){
       $scope.frases = response.data;
   })};
 
@@ -31,10 +38,10 @@ setInterval(buscarMensagens(), 3000);
 
 
   if (window.localStorage.getItem("nome")=== null){
-  let   usuario = prompt ("Digite seu nome.");
-  let foto = prompt("Insira uma foto.");
-  window.localStorage.setItem("nome", usuario)
-  window.localStorage.setItem("img", foto)
-  $scope.url = foto;
+      let   usuario = prompt ("Digite seu nome.");
+      let foto = prompt("Insira uma foto.");
+      window.localStorage.setItem("nome", usuario)
+      window.localStorage.setItem("img", foto)
+      $scope.url = foto;
   }
 });
