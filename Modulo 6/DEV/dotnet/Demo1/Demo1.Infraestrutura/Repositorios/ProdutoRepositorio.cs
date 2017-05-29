aa88e19f6bc140c4edc139a414d9c7397995039a
mateusforgiarini <mateus.silva@cwi.com.br>
2017-05-29 17:41:12 -0300

    feat (dia4) implementar tarefa feita em aula.

---
+++ Modulo 6/DEV/dotnet/Demo1/Demo1.Infraestrutura/Repositorios/ProdutoRepositorio.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+using Demo1.Dominio.Entidades;
+
+namespace Demo1.Infraestrutura.Repositorios
+{
+    public class ProdutoRepositorio
+    {
+        string stringConexao = @"Server=13.65.101.67; User Id=mateus.silva; Password=123456; Database=aluno20db";
+
+        public void Criar(Produto produto)
+        {
+
+            using (var conexao = new SqlConnection(stringConexao))
+            {
+                conexao.Open();
+
+                using (var comando = conexao.CreateCommand())
+                {
+                    comando.CommandText = @"INSERT INTO Produto(Nome, Preco, Estoque) VALUES (@nome, @preco, @estoque)";
+
+                    comando.Parameters.AddWithValue("@nome", produto.Nome);
+                    comando.Parameters.AddWithValue("@preco", produto.Preco);
+                    comando.Parameters.AddWithValue("@estoque", produto.Estoque);
+
+                    // retorna somente a quantidade de linhas 
+                    comando.ExecuteNonQuery();
+
+
+                    // lê o resultado de um select 
+                    // comando.ExecuteXmlReader();
+                    // executa o comando e retorna o primeiro resultado
+                    //
+
+
+
+                }
+                using (var comando = conexao.CreateCommand())
+                {
+                    comando.CommandText = "SELECT @@IDENTITY";
+                    var result = (decimal)comando.ExecuteScalar();
+                    produto.Id = (int)result;
+
+                }
+            }
+
+
+        }
+
+        public List<Produto> Listar()
+        {
+            var produtos = new List<Produto>();
+            using (var conexao = new SqlConnection(stringConexao))
+            {
+                conexao.Open();
+
+
+                using (var comando = conexao.CreateCommand())
+                {
+                    comando.CommandText =
+                        "Select id, Nome, Preco, Estoque from Produto";
+
+                    var dataReader = comando.ExecuteReader();
+                    while (dataReader.Read())
+                    {
+                        var produto = new Produto();
+
+                        produto.Id = (int)dataReader["Id"];
+                        produto.Nome = (string)dataReader["Nome"];
+                        produto.Preco = (decimal)dataReader["Preco"];
+                        produto.Estoque = (int)dataReader["Estoque"];
+
+                        produtos.Add(produto);
+                    }
+                }
+            }
+
+
+                return produtos;
+        }
+
+
+    }
+}
---
+++ Modulo 6/DEV/dotnet/Demo1/Demo1.WebAP1/Controllers/ProdutosController.cs
@@ -0,0 +1,42 @@
+﻿using Demo1.Dominio.Entidades;
+using Demo1.Infraestrutura.Repositorios;
+
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace Demo1.WebAP1.Controllers
+{
+    public class ProdutosController : ApiController
+    {
+        ProdutoRepositorio _produtoRepositorio = new ProdutoRepositorio();
+
+        public IHttpActionResult Post(Produto produto)
+        {
+
+            var mensagens = new List<string>();
+            if (!produto.Validar(out mensagens))
+            {
+                return BadRequest(string.Join(".", mensagens.ToArray()));
+            }
+            
+                return Ok(produto);
+        }
+
+        public IHttpActionResult Get()
+        {
+            var produtos = _produtoRepositorio.Listar();
+            return Ok(produtos);
+        }
+
+
+    }
+
+
+
+}
+
