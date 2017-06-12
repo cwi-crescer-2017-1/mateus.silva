0a214a81282f57b901138c581dce90fd512198ff
mateusforgiarini <mateus.silva@cwi.com.br>
2017-06-12 18:19:55 -0300

    feat (aula1) implementar exercício 2.

--- Modulo 7/CRESCER 2017 -Aula1.sql
+++ Modulo 7/CRESCER 2017 -Aula1.sql
@@ -1,3 +1,4 @@
+--1
 SELECT idproduto, nome FROM produto WHERE idproduto not in(
 SELECT t1.idproduto
 FROM produto t1 JOIN pedidoitem t2
@@ -6,3 +7,16 @@ WHERE  t2.idpedido  in (
 SELECT idpedido FROM pedido WHERE  datapedido
 >(trunc(sysdate) -120)))
 order by idproduto;
+
+--2
+UPDATE  produto SET situacao =  'I' WHERE  idproduto not in(
+SELECT t1.idproduto
+FROM produto t1 JOIN pedidoitem t2
+ON t1.idproduto = t2.idproduto
+WHERE  t2.idpedido  in (
+SELECT idpedido FROM pedido WHERE  datapedido
+>(trunc(sysdate) -120)));
+commit;
+
+--3
+
