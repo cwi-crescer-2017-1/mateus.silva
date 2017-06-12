fdf81d0edc15d045a62699a8d7eb0b450774a281
mateusforgiarini <mateus.silva@cwi.com.br>
2017-06-12 18:14:00 -0300

    fix (aula1) corrigir exercício 1.

--- Modulo 7/CRESCER 2017 -Aula1.sql
+++ Modulo 7/CRESCER 2017 -Aula1.sql
@@ -2,7 +2,7 @@ SELECT idproduto, nome FROM produto WHERE idproduto not in(
 SELECT t1.idproduto
 FROM produto t1 JOIN pedidoitem t2
 ON t1.idproduto = t2.idproduto
-where t2.idpedido  in (
-select idpedido from pedido where datapedido
-BETWEEN trunc(sysdate) -120 AND trunc(sysdate)))
-GROUP BY idproduto, nome;
+WHERE  t2.idpedido  in (
+SELECT idpedido FROM pedido WHERE  datapedido
+>(trunc(sysdate) -120)))
+order by idproduto;
