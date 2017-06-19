e7e7bdf4e7ff7cb50139190cd1faf599151ace95
mateusforgiarini <mateus.silva@cwi.com.br>
2017-06-19 08:00:26 -0300

    fix (megasena) corrigir código.

--- Modulo 7/tarefa/MEGASENA.sql
+++ Modulo 7/tarefa/MEGASENA.sql
@@ -24,78 +24,72 @@ create or replace PROCEDURE atualizaAcertadores (pConcurso in integer) as
 varPremioQuadra concurso.premio_quadra%type;
 varPremioQuina concurso.premio_quadra%type;
 varPremioSena concurso.premio_quadra%type;
-varQtdAcertadores4Numero integer;
-varQtdAcertadores5Numero integer;
-varQtdAcertadores6Numero integer;
-varacumulou integer;
-varacertos integer;
-varPrimeiraDezena integer;
-varSegundaDezena integer;
-varTerceiraDezena integer;
-varQuartaDezena integer;
-varQuintaDezena integer;
-varSextaDezena integer;
- 
-CURSOR apostas IS 
-  select*from numero_aposta where idaposta in (select idaposta   from aposta  where idconcurso =pconcurso);
- 
-Cursor byaposta IS 
- select idaposta from (select*from numero_aposta where idaposta in (select idaposta   from aposta  where idconcurso =pconcurso))
- group by idaposta;
- 
-begin
+varQtdAcertadores4Numero number;
+varQtdAcertadores5Numero number;
+varQtdAcertadores6Numero number;
+varacumulou concurso.acumulou%type;
+varacertos aposta_premiada.acertos%type;
+varPrimeiraDezena concurso.primeira_dezena%type;
+varSegundaDezena concurso.primeira_dezena%type;
+varTerceiraDezena concurso.primeira_dezena%type;
+varQuartaDezena concurso.primeira_dezena%type;
+varQuintaDezena concurso.primeira_dezena%type;
+varSextaDezena concurso.primeira_dezena%type;
+CURSOR apostas  (pIdAposta in number) is
+           select  numero 
+            from numero_aposta
+            where IdAposta = pIdAposta;
 
-SELECT premio_quadra into varPremioQuadra FROM concurso where idconcurso = pConcurso;
-SELECT premio_quina into varPremioQuina FROM concurso where idconcurso = pConcurso;
-SELECT premio_sena into varPremioSena FROM concurso where idconcurso = pConcurso;
-
-select primeira_dezena into varPrimeiraDezena from concurso where idconcurso = pConcurso;
-select segunda_dezena into varSegundaDezena from concurso where idconcurso = pConcurso;
-select terceira_dezena into varTerceiraDezena from concurso where idconcurso = pConcurso;
-select quarta_dezena into varQuartaDezena from concurso where idconcurso = pConcurso;
-select quinta_dezena into varQuintaDezena from concurso where idconcurso = pConcurso;
-select sexta_dezena into varSextaDezena from concurso where idconcurso = pConcurso;
-
-
-FOR a  in byaposta LOOP
-    FOR b in apostas LOOP
-    IF(a.idaposta = b.idaposta) THEN
-         IF (b.numero = varPrimeiraDezena)THEN
+  Cursor byaposta 
+ (pConcurso in number) is
+            select IdAposta
+            from aposta
+            where IdConcurso = pConcurso; 
+BEGIN
+SELECT premio_quadra into varPremioQuadra FROM concurso where idconcurso =pConcurso;
+SELECT premio_quina into varPremioQuina FROM concurso where idconcurso =pConcurso;
+SELECT premio_sena into varPremioSena FROM concurso where idconcurso =pConcurso;
+SELECT primeira_dezena into varPrimeiraDezena from concurso where idconcurso = pConcurso;
+SELECT segunda_dezena into varSegundaDezena from concurso where idconcurso = pConcurso;
+SELECT terceira_dezena into varTerceiraDezena from concurso where idconcurso = pConcurso;
+SELECT quarta_dezena into varQuartaDezena from concurso where idconcurso = pConcurso;
+SELECT quinta_dezena into varQuintaDezena from concurso where idconcurso = pConcurso;
+SELECT sexta_dezena into varSextaDezena from concurso where idconcurso = pConcurso;
+FOR aposta  in byaposta (pConcurso)LOOP
+ varacertos:=0;
+    FOR apostador in apostas (aposta.IdAposta )LOOP    
+         IF (apostador.numero = varPrimeiraDezena)THEN
              varacertos := varacertos+1;
-         ELSIF (b.numero = varSegundaDezena)THEN
+         ELSIF (apostador.numero = varSegundaDezena)THEN
              varacertos := varacertos+1;
-         ELSIF (b.numero = varTerceiraDezena)THEN
+         ELSIF (apostador.numero = varTerceiraDezena)THEN
               varacertos := varacertos+1;
-         ELSIF (b.numero = varQuartaDezena)THEN
+         ELSIF (apostador.numero = varQuartaDezena)THEN
               varacertos := varacertos+1;
-          ELSIF (b.numero = varQuintaDezena)THEN
+          ELSIF (apostador.numero = varQuintaDezena)THEN
               varacertos := varacertos+1;
-          ELSIF (b.numero = varSextaDezena)THEN
+          ELSIF (apostador.numero = varSextaDezena)THEN
               varacertos := varacertos+1;
          END IF;
-      END IF; 
     END LOOP;
       IF (varacertos =4)THEN
-          update aposta_premiada set acertos =4 where idaposta = a.idaposta;
+        Insert into aposta_premiada (idaposta, acertos, valor) values (aposta.idaposta, 4, 0);
       ELSIF (varacertos =5)THEN
-         update aposta_premiada set acertos =5 where idaposta = a.idaposta;
+         Insert into aposta_premiada (idaposta, acertos, valor) values (aposta.idaposta, 5, 0);
       ELSIF (varacertos =6)THEN
-         update aposta_premiada set acertos =6 where idaposta = a.idaposta;
+         Insert into aposta_premiada (idaposta, acertos, valor) values (aposta.idaposta, 6, 0);
       END IF;
-      varacertos:=0;
-END LOOP;
+  END LOOP;
 select count(acertos) into varQtdAcertadores4Numero  from aposta_premiada where acertos=4;
 select count(acertos) into varQtdAcertadores5Numero from aposta_premiada where acertos=5;
 select count(acertos) into varQtdAcertadores6Numero  from aposta_premiada where acertos=6;
-
 Update aposta_premiada set valor = varPremioQuadra/varQtdAcertadores4Numero where acertos = 4;
 Update aposta_premiada set valor = varPremioQuina/varQtdAcertadores5Numero where acertos = 5;
 Update aposta_premiada set valor = varPremioSena/varQtdAcertadores6Numero where acertos = 6;
-
 select  count(acertos)into varacumulou  from aposta_premiada where acertos = 6;
     IF (varacumulou>0)THEN
-        UPDATE concurso set acumulou = 'não' where idconcurso = pConcurso;
-     ELSE 
-        UPDATE concurso set acumulou = 'sim' where idconcurso = pConcurso;
+        UPDATE concurso set acumulou = 1 where idconcurso = pConcurso;
+     ELSE
+        UPDATE concurso set acumulou = 0 where idconcurso = pConcurso;
      END IF;
  END atualizaAcertadores;
