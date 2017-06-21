32d22bd1ff9144bc84bb39b1fad04a1ec59f6a9a
mateusforgiarini <mateus.silva@cwi.com.br>
2017-06-20 23:31:36 -0300

    feat (tema1) implementar Meu Calendario Utils.

---
+++ Modulo 8/tema1/src/main/java/CalendarUtils.java
@@ -0,0 +1,30 @@
+
+import java.util.Date;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Mateus
+ */
+public interface CalendarUtils {
+
+    public enum DiaSemana {
+        DOMINGO,
+        SEGUNDA_FEIRA,
+        TERCA_FEIRA,
+        QUARTA_FEIRA,
+        QUINTA_FEIRA,
+        SEXTA_FEIRA,
+        SABADO;
+    }
+
+    DiaSemana diaSemana(Date date);
+
+    String tempoDecorrido(Date date);
+
+}
---
+++ Modulo 8/tema1/src/main/java/ManipuladorDeDatas.java
@@ -0,0 +1,35 @@
+
+import java.util.Calendar;
+import java.util.Date;
+import java.util.Locale;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Mateus
+ */
+public class ManipuladorDeDatas implements CalendarUtils{
+    
+   public  DiaSemana diaSemana(Date date){
+      Calendar calendar = Calendar.getInstance(); 
+      calendar.setTime(date);
+      return DiaSemana.values()[calendar.get(Calendar.DAY_OF_WEEK)];
+   }
+
+   public  String tempoDecorrido(Date date){
+      Calendar data = Calendar.getInstance(); 
+      data.setTime(date);
+      Calendar hoje = Calendar.getInstance(); 
+      hoje.setTime(new Date());
+      
+      int ano = hoje.get(Calendar.YEAR) -data.get(Calendar.YEAR);
+      int mes = hoje.get(Calendar.MONTH) -data.get(Calendar.MONTH);
+      int dia = hoje.get(Calendar.DATE) -data.get(Calendar.DATE);
+   return ano +" ano(s), " + mes + " mes(es) e " + dia + " dia(s)";
+   }
+}
